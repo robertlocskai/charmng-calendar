5854cb19443eddbae7127b47b036d40886ed41b5
localwebdev <laszlo.csonka13@gmail.com>
2025-01-13 20:59:31 +0100

    fix: added interface export in public-api

--- projects/charmng-calendar/src/public-api.ts
+++ projects/charmng-calendar/src/public-api.ts
@@ -6,3 +6,4 @@ export * from './lib/charmng-calendar.service';
 export * from './lib/charmng-calendar.component';
 export * from './lib/calendar/calendar.component';
 export * from './lib/charmng-calendar.module';
+export * from './lib/interfaces/calendar.event.interface';
